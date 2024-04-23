6a469ad31aabff457656c9898c5660e74b1e7910
Juan Fernando Contreras <157652760+TheMarioDance@users.noreply.github.com>
2024-04-23 16:37:23 -0500

    help

--- pset-3/code/202011873.R
+++ pset-3/code/202011873.R
@@ -21,6 +21,8 @@ p_load(rio, # funcion import/export: permite leer/escribir archivos desde difere
        dplyr,
        data.table) # renombar variables
 
+setwd("~/GitHub/problem-sets/pset-3")
+
 # Listar todos los archivos en la carpeta 'input' y subcarpetas
 archivos <- list.files("input",full.names = T, recursive = T)
 
@@ -30,8 +32,27 @@ lista_datos <- list()
 
 
 for (i in archivos) {
-  datos <- readRDS(i)
-  lista_datos[[i]] <- datos
+ 
+  lista_datos[[i]] <- readRDS(i)
   
 }
 
+caracteristicas_generales <- rbindlist(lista_datos[grep("Caracteristicas generales", names(lista_datos))], fill = TRUE, use.names = TRUE)
+
+fuerzatrabajo <- rbindlist(lista_datos[grep("Fuerza de trabajo", names(lista_datos))], fill = TRUE, use.names = TRUE)
+
+noocup <- rbindlist(lista_datos[grep("No ocupados", names(lista_datos))], fill = TRUE, use.names = TRUE)
+
+ocup <- rbindlist(lista_datos[grep("Ocupados", names(lista_datos))], fill = TRUE, use.names = TRUE)
+
+resultado <- fuerzatrabajo %>%
+  # Agrupar datos por mes
+  group_by(MES) %>%
+  # Sumar el factor de expansión para la fuerza de trabajo y la población en edad de trabajar
+  summarise(
+    Suma_FT = sum(factor_expansion[FT == 1], na.rm = TRUE),  # Suma de factor de expansión para la fuerza laboral
+    Suma_PET = sum(factor_expansion[PET == 1], na.rm = TRUE)  # Suma de factor de expansión para la población en edad de trabajar
+  )
+
+# Mostrar el resultado
+print(resultado)
